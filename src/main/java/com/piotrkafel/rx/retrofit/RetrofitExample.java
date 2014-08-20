cb32ebda9b3f9de6a1fd723ba7e0de5062223efa
pkafel <piotr.kafel@gmail.com>
2014-08-20 22:49:45 +0200

    Var names fix

--- src/main/java/com/piotrkafel/rx/retrofit/RetrofitExample.java
+++ src/main/java/com/piotrkafel/rx/retrofit/RetrofitExample.java
@@ -28,16 +28,16 @@ public class RetrofitExample {
 
     public UserProfile getUserProfileExample(UUID userId) {
 
-        final Observable<UserData> userData = userClient.getUser(userId);
+        final Observable<UserData> requestedUserData = userClient.getUser(userId);
 
-        final Observable<UserContacts> userContacts = contactClient.getContactsByUserId(userId)  // get user's contacts
-                .flatMap( contacts -> Observable.from(contacts.getContacts()) )                  // unwrap contacts object so we will have Observable that emits many items
-                .mergeMap( contact -> userClient.getUser(contact.getUserId() ),                  // for each contact call user client in order to get details
-                          (jsonHolder, jsonElement) -> new UserContact(jsonHolder, jsonElement)) // and zip contact with user data (or do with it whatever you like)
-                .toList()                                                                        // transform back to list
-                .map( userContactsList -> new UserContacts(userContactsList) );                  // wrap list of user contacts into UserContacts object
+        final Observable<UserContacts> requestedUserContacts = contactClient.getContactsByUserId(userId)    // get user's contacts
+                .flatMap( contacts -> Observable.from(contacts.getContacts()) )                             // unwrap contacts object so we will have Observable that emits many items
+                .mergeMap( contact -> userClient.getUser(contact.getUserId()),                              // for each contact call user client in order to get details
+                        (contact, userData) -> new UserContact(contact, userData) )                         // and zip contact with user data (or do with it whatever you like)
+                .toList()                                                                                   // transform back to list
+                .map( userContactsList -> new UserContacts(userContactsList) );                             // wrap list of user contacts into UserContacts object
 
-        return Observable.zip(userData, userContacts, (userDataToZip, userContactsToZip) -> new UserProfile(userDataToZip, userContactsToZip))
+        return Observable.zip(requestedUserData, requestedUserContacts, (userData, userContacts) -> new UserProfile(userData, userContacts) )
                 .toBlocking().single(); // zip requested user data with all its contacts
     }
 }
