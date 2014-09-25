1947d7f1aa4ed21f25764d857d47d7a53355467c
pkafel <piotr.kafel@gmail.com>
2014-09-25 21:45:31 +0200

    Fancy predicate in amb test

--- src/test/java/com/piotrkafel/rx/simple/AmbExampleTest.java
+++ src/test/java/com/piotrkafel/rx/simple/AmbExampleTest.java
@@ -1,6 +1,11 @@
 package com.piotrkafel.rx.simple;
 
 import org.junit.Test;
+import rx.Observable;
+import rx.functions.Func1;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -17,10 +22,7 @@ public class AmbExampleTest {
         final Iterable<Integer> integers = new AmbExample().getNumbers(oddDelay, evenDelay);
 
         // Then
-        assertNotNull(integers);
-        for (Integer integer : integers) {
-            assertTrue(integer % 2 == 0);
-        }
+        assertOutput(integers, integer -> integer % 2 == 0);
     }
 
     @Test
@@ -33,9 +35,13 @@ public class AmbExampleTest {
         final Iterable<Integer> integers = new AmbExample().getNumbers(oddDelay, evenDelay);
 
         // Then
+        assertOutput(integers, integer -> integer % 2 != 0);
+    }
+
+    private void assertOutput(Iterable<Integer> integers, Predicate<Integer> predicate) {
         assertNotNull(integers);
         for (Integer integer : integers) {
-            assertTrue(integer % 2 != 0);
+            assertTrue(predicate.test(integer));
         }
     }
 }
