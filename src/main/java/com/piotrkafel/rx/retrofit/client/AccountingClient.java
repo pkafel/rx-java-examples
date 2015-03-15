c1bc768265594aa850517673838314d2b682936f
pkafel <piotr.kafel@gmail.com>
2015-03-15 21:19:43 +0100

    Update accounting client

--- src/main/java/com/piotrkafel/rx/retrofit/client/AccountingClient.java
+++ src/main/java/com/piotrkafel/rx/retrofit/client/AccountingClient.java
@@ -1,11 +1,17 @@
 package com.piotrkafel.rx.retrofit.client;
 
 import com.google.gson.JsonObject;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
 import rx.Observable;
 
 import java.util.UUID;
 
 public interface AccountingClient {
 
-    public Observable<JsonObject> getContractLineItems(UUID uuid, Integer page, Integer perPage);
+    @GET("/line_item/{uuid}")
+    public Observable<JsonObject> getContractLineItems(@Path("uuid") UUID uuid,
+                                                       @Query("page") Integer page,
+                                                       @Query("perPage") Integer perPage);
 }
